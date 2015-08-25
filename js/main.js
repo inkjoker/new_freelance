e011e6e7201495ebbd3e3f4a882d4b992707a71a
sergey.solaynik <sergey.solaynik@cactussoft.biz>
2015-08-25 11:48:07 +0300

    CUT-2455
    Signed-off-by: sergey.solaynik <sergey.solaynik@cactussoft.biz>

--- js/main.js
+++ js/main.js
@@ -1,7 +1,35 @@
 $(document).ready(function(){
-  $('.slider1').bxSlider({
-    minSlides: 1,
-    maxSlides: 1,
-    slideMargin: 10
-  });
+
+	var sliderFirst = $('.slider1').bxSlider({
+		minSlides: 1,
+		maxSlides: 1,
+		slideMargin: 10,
+		pager : false,
+		controls : false
+	});
+
+	$('#tottalSlide').text(sliderFirst.getSlideCount());
+
+	$('#slider-prev').click(function(){
+		sliderFirst.goToPrevSlide();
+		$('#currentSlide').text(sliderFirst.getCurrentSlide() + 1);
+		return false;
+	});
+
+	$('#slider-next').click(function(){
+		sliderFirst.goToNextSlide();
+		$('#currentSlide').text(sliderFirst.getCurrentSlide() + 1);
+		return false;
+	});
+
+	$('#slider-m-prev').click(function(){
+		sliderFirst.goToPrevSlide();
+		$('#currentSlide').text(sliderFirst.getCurrentSlide() + 1);
+		return false;
+	});
+	$('#slider-m-next').click(function(){
+		sliderFirst.goToNextSlide();
+		$('#currentSlide').text(sliderFirst.getCurrentSlide() + 1);
+		return false;
+	});
 });
