3c0f9cc0498b1de6018ef6ffecb944de8000a605
sergey.solaynik <sergey.solaynik@cactussoft.biz>
2015-08-28 09:36:35 +0300

    CUT-2345
    Signed-off-by: sergey.solaynik <sergey.solaynik@cactussoft.biz>

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 
+	var index = 0;
+
 	var sliderFirst = $('.slider1').bxSlider({
 		minSlides: 1,
 		maxSlides: 1,
@@ -27,9 +29,59 @@ $(document).ready(function(){
 		$('#currentSlide').text(sliderFirst.getCurrentSlide() + 1);
 		return false;
 	});
+
 	$('#slider-m-next').click(function(){
 		sliderFirst.goToNextSlide();
 		$('#currentSlide').text(sliderFirst.getCurrentSlide() + 1);
 		return false;
 	});
+
+	$('#menuTop').click(function(e){
+
+		index = 0;
+		$('#nextScreen').show().attr('href', '#h.opt0');
+
+		scrollToAnchor(this, e);
+	});
+
+	$('.b-navigation_link').click(function(e){
+
+		var href = $(this).attr('href');
+		index = parseInt(href.match(/\d+$/)[0], 10);
+
+		$('#nextScreen').show().attr('href', index);
+
+		if (index == 4) {
+
+			$('#nextScreen').hide();
+		};
+
+		scrollToAnchor(this, e);
+	});
+
+	$('#nextScreen').click(function(e){
+
+		++index;
+		$(this).attr('href', '#h.opt' + index);
+
+		if (index == 4) {
+
+			$(this).hide();
+		};
+
+		scrollToAnchor(this, e);
+	});
+
+	function scrollToAnchor(scope, e) {
+
+		var name = $(scope).attr('href').replace(/\#/g, "");
+
+        $('html, body').stop().animate({
+
+            scrollTop: $("a[name='" + name + "']").offset().top
+
+        }, 1000);
+
+        e.preventDefault();
+	}
 });
